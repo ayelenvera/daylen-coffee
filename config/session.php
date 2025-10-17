4efe61d041617acf63fb5a4a307a1cd87b4de795
ayelenvera <ayeleennvera@gmail.com>
2025-10-17 11:46:47 -0300

    fix: configurar session domain null y secure true para railway

--- config/session.php
+++ config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'localhost'),
+    'domain' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
